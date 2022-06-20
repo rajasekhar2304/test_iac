3d56183b9e6b3825fc9e77be8e7ef71734d052fa
raajasekharr@gmail.com <raajasekharr@gmail.com>
2022-06-20 15:46:15 +0530

    Added exec command in jenkinsfile

--- script.sh
+++ script.sh
@@ -2,5 +2,5 @@
 #delete old files
 
 ls -l /home/devrabbit/Desktop/DevOps/shell_scripting/test
-sudo find /home/devrabbit/Desktop/DevOps/shell_scripting/test -type f -name '*.log' -mmin +30 -delete | sort
+find /home/devrabbit/Desktop/DevOps/shell_scripting/test -type f -name '*.log' -mmin +30 -exec sudo rm -rf {} \;
 ls -l /home/devrabbit/Desktop/DevOps/shell_scripting/test
