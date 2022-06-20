18e5fb77aacdca3b4c8351be9814472e7d3bb806
raajasekharr@gmail.com <raajasekharr@gmail.com>
2022-06-20 15:54:10 +0530

    Execution command deleted

--- script.sh
+++ script.sh
@@ -2,5 +2,5 @@
 #delete old files
 
 ls -l /home/devrabbit/Desktop/DevOps/shell_scripting/test
-find /home/devrabbit/Desktop/DevOps/shell_scripting/test -type f -name '*.log' -mmin +30 -exec sudo rm -rf {} \;
+find /home/devrabbit/Desktop/DevOps/shell_scripting/test -type f -name '*.log' -mmin +30 -delete | sort
 ls -l /home/devrabbit/Desktop/DevOps/shell_scripting/test
