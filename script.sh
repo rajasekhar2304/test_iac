fbbf9e1938d775641e2ca2dc8878fd76db2a16dc
raajasekharr@gmail.com <raajasekharr@gmail.com>
2022-06-20 15:23:25 +0530

    added script file and updated jenkinsfile

---
+++ script.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+#delete old files
+
+ls -l /home/devrabbit/Desktop/DevOps/shell_scripting/test
+find /home/devrabbit/Desktop/DevOps/shell_scripting/test -type f -name '*.log' -mmin +30 -delete | sort
+ls -l /home/devrabbit/Desktop/DevOps/shell_scripting/test
